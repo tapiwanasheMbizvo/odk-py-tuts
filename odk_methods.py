0d6c7941d5ada58e8d67bfcf0b03ea468e970ba9
Tapiwanashe Mbizvo <tmbizvo@ophid-trust.local>
2022-04-13 06:41:49 +0200

    done printing submissions

--- odk_methods.py
+++ odk_methods.py
@@ -1,13 +1,40 @@
 import requests
 import json
 import config
-#submission_response = requests.get(FORMSERVER_URL+'/projects/'+project_id+'/forms/'+form_name+'/submissions.csv.zip?attachments=false?media=true&%24filter=__system%2FsubmissionDate%20gt%20'+startdate+'%20and%20__system%2FsubmissionDate%20lt%202'+enddate,auth=HTTPBasicAuth(FORMSERVER_USER, FORMSERVER_PASSWORD))
+
+
+# submission_response = requests.get(FORMSERVER_URL+'/projects/'+project_id+'/forms/'+form_name+'/submissions.csv.zip
+# ?attachments=false?media=true&%24filter=__system%2FsubmissionDate%20gt%20'+startdate+'%20and%20__system
+# %2FsubmissionDate%20lt%202'+enddate,auth=HTTPBasicAuth(FORMSERVER_USER, FORMSERVER_PASSWORD))
 class ODKMETHODS():
     BASE_URL = config.BASE_URL
     SERVER_USER = config.SERVER_USER
     SERVER_PASSWORD = config.SERVER_PASSWORD
 
+    def getSubmissions(self, project_id, form_name, startdate, enddate):
+        """
+        Get all submissions from the server.
+        """
 
+        # Set the URL
+        url = self.BASE_URL + "/projects/" + project_id + "/forms/" + form_name + "/submissions.csv.zip?attachments=false?media=true&%24filter=__system%2FsubmissionDate%20gt%20" + startdate + "%20and%20__system%2FsubmissionDate%20lt%202" + enddate
+
+        # Set the auth
+        auth = (self.SERVER_USER, self.SERVER_PASSWORD)
+
+        # Make the request
+        print("making the request")
+        response = requests.get(url, auth=auth)
+
+        print("DONE.... checking for content ")
+        # Check the status code
+        if response.status_code == 200:
+            # Return the users
+            return response
+
+        else:
+            # Return an empty list
+            return []
 
     def getProjects(self):
         """
--- run.py
+++ run.py
@@ -1,8 +1,12 @@
-from odk_methods import ODKMETHODS
+from io import BytesIO
+from zipfile import ZipFile
 
+from odk_methods import ODKMETHODS
+import pandas as pd
 
 odk = ODKMETHODS()
 
+"""
 projects = odk.getProjects()
 for project in projects:
     project_name = project['name']
@@ -15,3 +19,25 @@ print(one_project)
 forms = odk.getForms(str(6))
 for form in forms:
     print(f"Form: {form['name']}\t State: {form['state']}")
+
+#  project_id, form_name, startdate, enddate):
+#  odk.getFormData(str(6), 'Form_1', '2019-01-01', '2019-01-31')
+"""
+response = odk.getSubmissions(project_id=str(6), form_name="TASQC COA's Diary of Daily Services v2",
+                              startdate="2022-01-01", enddate="2022-03-01")
+
+if response.content:
+    print("HEY HEY ")
+    df = pd.DataFrame()
+    result = pd.DataFrame()
+
+    zip_file = ZipFile(BytesIO(response.content))
+    files = zip_file.namelist()
+    if len(files) == 1:
+        file = files[0]
+        print(file)
+        df = pd.read_csv(zip_file.open(file))
+        print(df.head())
+        result = df.groupby('SubmissionDate').agg({'SubmitterName': 'count'})
+        print(result.head())
+        result.to_csv('submission_count.csv')
